df7aa04a4a13edada728136e9c6b8aa5445a7e9a
Николай Генералов <xsnakee@gmail.com>
2020-02-26 12:00:24 +0300

    Add project files.

---
+++ TestAppForLinuxServer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestAppForLinuxServer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
